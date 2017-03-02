328dde1d1612f1c29f01c6addf0e7f90aef6fe19
DaubaKao <ab6060118@gmail.com>
2017-03-02 10:20:59 +0800

    modify some mistake

--- installJSCompleter.sh
+++ installJSCompleter.sh
@@ -1,5 +1,5 @@
 sudo wget https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
 sudo tar -xvf node-v6.10.0-linux-x64.tar.xz
 sudo rm node-v6.10.0-linux-x64.tar.xz
-sudo mv node-v6.10.0-linux-x64.tar.xz /opt/node
+sudo mv node-v6.10.0-linux-x64 /opt/node
 cd ~/.vim/bundle/tern_for_vim && npm install
