04f145a34bd3f7e8b226aed78f0b981bbc191fcd
DaubaKao <ab6060118@gmail.com>
2017-03-02 10:01:08 +0800

    change node js version

--- installJSCompleter.sh
+++ installJSCompleter.sh
@@ -1,5 +1,5 @@
-sudo wget https://nodejs.org/dist/v4.4.5/node-v4.4.5-linux-x64.tar.xz
-sudo tar -xvf node-v4.4.5-linux-x64.tar.xz
-sudo rm node-v4.4.5-linux-x64.tar.xz
-sudo mv node-v4.4.5-linux-x64 /opt/node
+sudo wget https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
+sudo tar -xvf node-v6.10.0-linux-x64.tar.xz
+sudo rm node-v6.10.0-linux-x64.tar.xz
+sudo mv node-v6.10.0-linux-x64.tar.xz
 cd ~/.vim/bundle/tern_for_vim && npm install
