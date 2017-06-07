0069b14268014f41ca35c330dde360bdeb78de00
DaubaKao <ab6060118@gmail.com>
2017-06-07 23:41:50 +0800

    [update]

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -2,13 +2,13 @@
 
 ./installZsh.sh
 
-cp ./.vimrc ~
-cp ./.tmux.conf ~
-cp ./.inputrc ~
-cp -r ./.vim ~
-cp ./.gitconfig ~
-cp ./.zshrc ~
-cp ./daubakao.zsh-theme ~/.oh-my-zsh/themes
+ln -s $PWD/.vimrc $HOME
+ln -s $PWD/.tmux.conf $HOME
+ln -s $PWD/.inputrc $HOME
+ln -s $PWD/.vim $HOME
+ln -s $PWD/.gitconfig $HOME
+ln -sf $PWD/.zshrc $HOME
+ln -s $PWD/daubakao.zsh-theme $HOME/.oh-my-zsh/themes
 
 vim -c PluginInstall -c qa
 
--- installJSCompleter.sh
+++ installJSCompleter.sh
@@ -1,5 +1,5 @@
-wget https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.xz
-tar -xvf node-v6.10.0-linux-x64.tar.xz
-rm node-v6.10.0-linux-x64.tar.xz
-sudo mv node-v6.10.0-linux-x64 /opt/node
+wget https://nodejs.org/dist/v8.0.0/node-v8.0.0-linux-x64.tar.xz
+tar -xvf node-v8.0.0-linux-x64.tar.xz
+rm node-v8.0.0-linux-x64.tar.xz
+sudo mv node-v8.0.0-linux-x64 /opt/node
 cd ~/.vim/bundle/tern_for_vim && npm install
