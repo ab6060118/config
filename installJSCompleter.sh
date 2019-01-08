9a521332f18e96c687f51338d3fdc0023a3e5418
DaubaKao <ab6060118@gmail.com>
2019-01-08 15:07:26 +0800

    update installJSComplete.sh

--- installJSCompleter.sh
+++ installJSCompleter.sh
@@ -1,13 +1,13 @@
 NODE_INSTALL_PATH=/opt/node
 NODE_BIN_PATH=$NODE_INSTALL_PATH/bin
 
-wget https://nodejs.org/dist/v8.0.0/node-v8.0.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v10.15.0/node-v10.15.0-linux-x64.tar.xz
 
-tar -xvf node-v8.0.0-linux-x64.tar.xz
+tar -xvf node-v10.15.0-linux-x64.tar.xz
 
-rm node-v8.0.0-linux-x64.tar.xz
+rm node-v10.15.0-linux-x64.tar.xz
 
-sudo mv node-v8.0.0-linux-x64 $NODE_INSTALL_PATH
+sudo mv node-v10.15.0-linux-x64 $NODE_INSTALL_PATH
 
 cd ~/.vim/bundle/tern_for_vim
 npm install
